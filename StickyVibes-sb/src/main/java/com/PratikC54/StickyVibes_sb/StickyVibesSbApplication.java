7f1d920ea14ed8e0b98ff0822b1ddefe6242be7b
PratikC54 <pratiksenapati54@gmail.com>
2026-06-24 21:15:47 +0530

    Setting backend for project

---
+++ StickyVibes-sb/src/main/java/com/PratikC54/StickyVibes_sb/StickyVibesSbApplication.java
@@ -0,0 +1,13 @@
+package com.PratikC54.StickyVibes_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickyVibesSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickyVibesSbApplication.class, args);
+	}
+
+}
