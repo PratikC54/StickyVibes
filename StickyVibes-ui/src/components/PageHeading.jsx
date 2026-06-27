47468a4126f54a62d60814af96622a2c0c3db8bf
PratikC54 <pratiksenapati54@gmail.com>
2026-06-27 22:46:56 +0530

    creating page heading section

---
+++ StickyVibes-ui/src/components/PageHeading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageTitle from './PageTitle'
+
+function PageHeading() {
+  return (
+    <div className="page-heading-container">
+      <PageTitle title="Explore StickyVibes"/>
+      <p>Discover the best stickers for your needs.</p>
+    </div>
+  )
+}
+
+export default PageHeading
